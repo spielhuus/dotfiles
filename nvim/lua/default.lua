6a9b65cc30cf66fd1c1f2eac22ccd3c0ec2fc7e2
spielhuus <spielhuus@gmail.com>
2024-04-09 16:46:20 +0200

    move back to brave, use bat and disable neotree

--- nvim/lua/default.lua
+++ nvim/lua/default.lua
@@ -67,3 +67,14 @@ opt.cmdheight = 0
 
 -- GUI
 -- opt.guifont = "Fira_Code:h14"
+--
+
+-- icons for diagnostic errors:
+vim.fn.sign_define("DiagnosticSignError",
+  { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn",
+  { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo",
+  { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint",
+  { text = "", texthl = "DiagnosticSignHint" })
