87d4f6591c8b41dedb3b6088901e37c6270dd6c3
spielhuus <spielhuus@gmail.com>
2024-05-27 06:53:18 +0200

    update files

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,6 +1,7 @@
 return ({
         {
                 "Mofiqul/dracula.nvim",
+                enabled = false,
                 lazy = false,
                 priority = 1000,
                 config = function()
