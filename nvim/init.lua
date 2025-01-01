261d10bead620b2b9e7167dae304d94d63f26800
spielhuus <spielhuus@gmail.com>
2025-01-01 11:04:51 +0100

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,22 +3,22 @@ require("autocommands")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-  change_detection = {
-    enabled = true,
-    notify = false,
-  },
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
 })
 
 require("breadcrumbs")
--- nvim/lua/plugins/dropbar.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "Bekaboo/dropbar.nvim",
-  enabled = true,
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-  },
-  opts = {},
-}
