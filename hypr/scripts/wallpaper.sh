a7eac02e136064b0359c5119565a25dfad6e2d0c
spielhuus <spielhuus@gmail.com>
2025-10-17 18:02:43 +0200

    add desktop theming

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -1,32 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# This script will randomly go through the files of a directory, setting it
-# up as the wallpaper at regular intervals
-#
-# NOTE: this script is in bash (not posix shell), because the RANDOM variable
-# we use is not defined in posix
-
-if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
-	echo "Usage:
-	$0 <dir containing images>"
-	exit 1
-fi
-
-# Edit below to control the images transition
-export SWWW_TRANSITION_FPS=60
-export SWWW_TRANSITION_STEP=2
-
-# This controls (in seconds) when to switch to the next image
-INTERVAL=300
+WALLPAPER_DIR="$HOME/Douments/wallpapers/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 while true; do
-	find "$1" -type f \
-		| while read -r img; do
-			echo "$((RANDOM % 1000)):$img"
-		done \
-		| sort -n | cut -d':' -f2- \
-		| while read -r img; do
-			swww img "$img"
-			sleep $INTERVAL
-		done
+  sleep 900
+  # Get a random wallpaper that is not the current one
+  WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+  # Apply the selected wallpaper
+  hyprctl hyprpaper reload ,"$WALLPAPER"
 done
