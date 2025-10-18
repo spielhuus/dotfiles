a39d8d5bb635a26ab7ca74c44f66dcbfaeddf0a8
spielhuus <spielhuus@gmail.com>
2025-10-18 16:41:40 +0200

    update hypr

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR="$HOME/Douments/wallpapers/"
+WALLPAPER_DIR="$HOME/Douments/Wallpapers/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 while true; do
-  sleep 900
+  sleep 60
   # Get a random wallpaper that is not the current one
   WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
   # Apply the selected wallpaper
